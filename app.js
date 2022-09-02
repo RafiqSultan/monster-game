d64dfa54bfbd688f838e0c1b46bdaa4bb08d2978
RafiqSultan <Rafiqaasultan@gmail.com>
2022-09-02 10:01:47 -0700

    ADD event attack to btn attack

--- app.js
+++ app.js
@@ -10,19 +10,28 @@ const app = Vue.createApp({
       playerHealth:100
     };
   },
+  computed:{
+    monsterBarStyle(){
+      return {width : this.monsterHealth + '%'};
+    },
+    playerBarStyle(){
+      return {width : this.playerHealth + '%'};
+    },
+  },
 
   methods: {
     attackMonster(){
       const attackValue= getRandomValue(5,12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
-
     },
     attackPlayer(){
       const attackValue = getRandomValue(8,15);
       this.playerHealth -= attackValue;
-    }
+    },
   },
 
 
 });
+
+app.mount('#game');
